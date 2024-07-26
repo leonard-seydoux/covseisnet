37b5f7faa9e2bc9d88835ddc25a8392a224db5d8
leonard-seydoux <leonard.seydoux@gmail.com>
2024-07-26 16:19:17 +0200

    Doc

--- covseisnet/plotting.py
+++ covseisnet/plotting.py
@@ -1,13 +1,28 @@
-"""Utilities to simplify the examples."""
+"""Utilities to simplify the examples.
+
+This module contains functions to simplify the examples in the documentation.
+
+Made by Leonard Seydoux in 2024.
+"""
 
 import matplotlib.pyplot as plt
 from scipy.fft import rfft, rfftfreq
 
 
 def plot_trace_and_spectrum(trace):
-    """Plot a trace and its spectrum."""
+    """Plot a trace and its spectrum.
+
+    Arguments
+    ---------
+    trace : obspy.Trace
+        The trace to plot.
+    """
     # Create figure
-    _, ax = plt.subplots(ncols=2, constrained_layout=True, figsize=(6, 3))
+    fig, ax = plt.subplots(ncols=2, constrained_layout=True, figsize=(6, 3))
+
+    # Calculate spectrum
+    spectrum = rfft(waveform)
+    frequencies = rfftfreq(len(waveform), trace.stats.delta)
 
     # Plot trace
     times = trace.times()
@@ -17,10 +32,10 @@ def plot_trace_and_spectrum(trace):
     ax[0].set_ylabel("Amplitude")
     ax[0].grid()
 
-    # Calculate spectrum
-    spectrum = rfft(waveform)
-    frequencies = rfftfreq(len(waveform), trace.stats.delta)
+    # Plot spectrum
     ax[1].loglog(frequencies, abs(spectrum))
     ax[1].set_xlabel("Frequency (Hz)")
     ax[1].set_ylabel("Spectrum")
     ax[1].grid()
+
+    return fig, ax
