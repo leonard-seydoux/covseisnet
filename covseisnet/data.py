d02dc3363644174931ec814ef0b1b174b160e412
leonard-seydoux <leonard.seydoux@gmail.com>
2024-07-29 16:06:03 +0200

    Split download and undervolc download + documented

--- covseisnet/data.py
+++ covseisnet/data.py
@@ -1,7 +1,47 @@
-"""Data management."""
+"""Data management.
 
+This module provides functions to download and manage seismic data. This is
+useful to download example datasets for instance.
+"""
+
+import obspy
 from obspy.clients.fdsn import Client
 from obspy import UTCDateTime
+
+
+def download_dataset(
+    starttime: UTCDateTime,
+    endtime: UTCDateTime,
+    datacenter: str,
+    **kwargs,
+) -> obspy.Stream:
+    """Download seismic data from a datacenter.
+
+    Arguments
+    ---------
+    starttime: :class:`~obspy.UTCDateTime`
+        The start time of the data to download.
+    endtime: :class:`~obspy.UTCDateTime`
+        The end time of the data to download.
+    datacenter: str
+        The datacenter to download the data from.
+    **kwargs: dict
+        Additional parameters to pass to the client.
+
+    Returns
+    -------
+    :class:`~obspy.Stream`
+        The downloaded seismic data.
+    """
+    # Client
+    client = Client(datacenter)
+
+    # Download data
+    return client.get_waveforms(
+        starttime=starttime,
+        endtime=endtime,
+        **kwargs,
+    )
 
 
 def download_undervolc_data(
@@ -11,7 +51,7 @@ def download_undervolc_data(
     datacenter: str = "RESIF",
     **kwargs,
 ) -> None:
-    """Download data from the UNDerVolc network.
+    """Download data from the UnderVolc network.
 
     Arguments
     ---------
@@ -23,9 +63,6 @@ def download_undervolc_data(
     # Print message
     print(f"Downloading data from the {datacenter} datacenter.")
 
-    # Client
-    client = Client(datacenter)
-
     # Set default parameters
     kwargs.setdefault("network", "YA")
     kwargs.setdefault("station", "UV*")
@@ -33,9 +70,7 @@ def download_undervolc_data(
     kwargs.setdefault("channel", "HHZ")
 
     # Download data
-    stream = client.get_waveforms(
-        starttime=starttime, endtime=endtime, **kwargs
-    )
+    stream = download_dataset(starttime=starttime, endtime=endtime, **kwargs)
 
     # Resample data to 20 Hz
     stream.resample(20)
