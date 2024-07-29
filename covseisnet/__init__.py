2d173aa00a6b2cc70ddc752a74fced6fe60c394d
leonard-seydoux <leonard.seydoux@gmail.com>
2024-07-29 18:19:58 +0200

    Added correlation to init

--- covseisnet/__init__.py
+++ covseisnet/__init__.py
@@ -2,7 +2,9 @@
 
 __all__ = [
     "calculate_covariance_matrix",
+    "calculate_cross_correlation_matrix",
     "CovarianceMatrix",
+    "CorrelationMatrix",
     "NetworkStream",
     "read",
     "plot",
@@ -12,6 +14,7 @@ __all__ = [
 ]
 
 from .covariance import CovarianceMatrix, calculate_covariance_matrix
+from .correlation import CorrelationMatrix, calculate_cross_correlation_matrix
 from .stream import NetworkStream, read
 from .signal import ShortTimeFourierTransform
 from . import signal
