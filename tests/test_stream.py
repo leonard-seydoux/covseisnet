35fe34b3a819dfa176df35d14f2ee44f1d925ea8
Leonard Seydoux <seydoux@ipgp.fr>
2024-08-04 07:54:13 +0200

    Added corresponding tests

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -6,14 +6,30 @@ from covseisnet import NetworkStream
 
 
 def test_stream_instance():
-    """Test that the read function returns a NetworkStream instance."""
+    """Read class method returns an instance of the NetworkStream class."""
     stream = NetworkStream.read()
     assert isinstance(stream, NetworkStream)
 
 
-# # Create an instance of the NetworkStream class
-# stream = read()
-# assert isinstance(stream, NetworkStream)
+def test_stream_station_property():
+    """The station property returns a list of stations."""
+    stream = NetworkStream.read()
+    assert len(stream.ids) == len(stream)
+
+
+def test_cut_with_endtime():
+    """Test trimming the stream with an endtime."""
+    stream = NetworkStream.read()
+    stream.cut("2009-08-24 00:20:05", "2009-08-24 00:20:12")
+    assert stream[0].stats.npts == 701
+
+
+def test_cut_with_duration():
+    """Test trimming the stream with an endtime."""
+    stream = NetworkStream.read()
+    stream.cut("2009-08-24 00:20:05", duration_sec=7)
+    assert stream[0].stats.npts == 701
+
 
 # # Check that there is only one station in the stream
 # stations = stream.stations
