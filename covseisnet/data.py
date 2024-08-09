ec53bd99ed14e17acf62061952e2a311703f7206
Leonard Seydoux <seydoux@ipgp.fr>
2024-08-09 10:09:22 +0200

    Documented data

--- covseisnet/data.py
+++ covseisnet/data.py
@@ -21,7 +21,7 @@ presented in this module, and run them in your own script.
     ├── covseisnet/
     ├── data/
     ├── docs/
-    ├── examples
+    ├── examples/
     ├── tests/
     ├── README.md
     ├── LICENSE
@@ -37,16 +37,12 @@ There are three presets to download data in the module:
 
 - :func:`~covseisnet.data.download_usarray_data` to download data from the US
   Transportable Array experiment between 2010-01-01 and 2010-03-01. In this
-  case, we download only the channels LHZ from the stations BGNE, J28A, sL27A,
-  N23A, O28A, P33A, R27A, S32A, U29A, W31A, allowing to show interesting
-  results of ambient-noise cross-correlation. 
+  case, we download only the channels LHZ from the stations R04C, O03C, M03C,
+  L02A, I05A, allowing to show interesting results of ambient-noise
+  cross-correlation. 
 
-- :func:`~covseisnet.data.download_geoscope_data` to download data from the
-  GEOSCOPE network between 2020-05-01 and 2020-08-01. This data is used to
-  show the results of ambient-noise cross-correlation at larger scale.
-
-These functions all call the :func:`~covseisnet.data.download_dataset` with
-specific arguments. You can also directly use these function to download
+These functions all call the :func:`~covseisnet.data.download_seismic_dataset`
+with specific arguments. You can also directly use these function to download
 datasets that you would like to try the package on. 
 
 """
@@ -54,112 +50,170 @@ datasets that you would like to try the package on.
 from os import path
 
 import obspy
-from obspy import UTCDateTime
+from obspy.core.utcdatetime import UTCDateTime
 from obspy.clients.fdsn import Client
+
+from .stream import map_processing_chain
 
 DIRECTORY_PACKAGE = path.dirname(__file__)
 DIRECTORY_DATA = path.join(path.dirname(DIRECTORY_PACKAGE), "data")
 
 
-def download_dataset(
-    starttime: UTCDateTime,
-    endtime: UTCDateTime,
+def download_seismic_dataset(
+    starttime: UTCDateTime | str,
+    endtime: UTCDateTime | str,
+    network: str,
+    station: str,
+    channel: str = "*",
+    location: str = "*",
     datacenter: str = "IRIS",
+    process: dict | None = None,
     **kwargs,
-) -> obspy.Stream | None:
+) -> obspy.Stream:
     """Download seismic data from a datacenter.
 
     This function is a simple wwrapper to the
     :meth:`~obspy.clients.fdsn.client.Client.get_waveforms` method. It connect
     to the FDSN client using the specified ``datacenter`` (which by default is
-    set to IRIS), and run the download with the
-    :meth:`~obspy.clients.fdsn.client.Client.get_waveforms` method between the
-    ``starttime`` and ``endtime``. Using the other arguments allow to specify
-    the query to the datacenter. For more information, please check the
-    Obspy documentation.
+    set to IRIS), and download seismic waveforms between the ``starttime`` and
+    ``endtime``. Using the other arguments allow to specify the query to the
+    datacenter. For more information, please check the Obspy documentation.
+
+    As we aim our package to be used without explicitely calling the Obspy
+    library, the dates are possible to be passed as strings.
+
+    Arguments
+    ---------
+    starttime: str or :class:`~obspy.core.utcdatetime.UTCDateTime`
+        The start time of the data to download.
+    endtime: str or :class:`~obspy.core.utcdatetime.UTCDateTime`
+        The end time of the data to download.
+    network: str
+        The network code to download the data from.
+    station: str
+        The station code to download the data from.
+    channel: str, optional
+        The channel code to download the data from. Default is "*".
+    location: str, optional
+        The location code to download the data from. Default is "*".
+    datacenter: str, optional
+        The datacenter to download the data from. Default is "IRIS".
+    process: dict, optional
+        A dictionary with the processing chain to apply to the downloaded
+        data. The keys of the dictionary are the processing functions to apply,
+        and the values are the arguments to pass to the processing function.
+    **kwargs: dict
+        Additional parameters to pass to the download method. These arguments
+        may vary depending on the datacenter.
+
+    Returns
+    -------
+    :class:`~obspy.core.stream.Stream`
+        The downloaded seismic data.
 
     Example
     -------
 
         >>> import covseisnet as csn
-        >>> from obspy import UTCDateTime
-        >>> stream = csn.download_dataset(
-                starttime=UTCDateTime("2010-01-01 00:00"),
-                endtime=UTCDateTime("2010-01-01 00:01"),
-                datacenter="IRIS",
-                channel="LHZ",
-                station="ANMO",
-                network=
+        >>> stream = csn.data.download_seismic_dataset(
+                starttime="2020-01-01 00:00",
+                endtime="2020-01-01 00:01",
+                channel="BHZ",
+                station="TAM",
+                network="G",
+                datacenter="IPGP",
             )
-
-    Arguments
-    ---------
-    starttime: :class:`~obspy.UTCDateTime`
-        The start time of the data to download.
-    endtime: :class:`~obspy.UTCDateTime`
-        The end time of the data to download.
-    datacenter: str
-        The datacenter to download the data from.
-    **kwargs: dict
-        Additional parameters to pass to the client.
-
-    Returns
-    -------
-    :class:`~obspy.Stream`
-        The downloaded seismic data.
+        >>> print(stream)
+        1 Trace(s) in Stream:
+        G.TAM.00.BHZ | 2020-01-01T00:00:03.350000Z - 2020-01-01T00:01:00.000000Z | 20.0 Hz, 1134 samples
     """
     # Client
     client = Client(datacenter)
 
+    # Turn string into UTCDateTime
+    starttime = UTCDateTime(starttime)
+    endtime = UTCDateTime(endtime)
+
     # Download data
-    return client.get_waveforms(
+    stream = client.get_waveforms(
         starttime=starttime,
         endtime=endtime,
+        network=network,
+        station=station,
+        channel=channel,
+        location=location,
         **kwargs,
     )
-
-
-def download_undervolc_data(
-    filepath_destination: str | None = None,
-    starttime: UTCDateTime = UTCDateTime("2010-10-14T09:00:00"),
-    endtime: UTCDateTime = UTCDateTime("2010-10-14T16:00:00"),
-    datacenter: str = "RESIF",
-    **kwargs,
-) -> None:
-    """Download data from the UnderVolc network.
-
-    Arguments
-    ---------
-    starttime : :class:`~obspy.UTCDateTime`
-        The start time of the data to download.
-    endtime : :class:`~obspy.UTCDateTime`
-        The end time of the data to download.
-    """
-    # Infer location
-    if filepath_destination is None:
-        filename = "example_undervolc.mseed"
-        filepath_destination = path.join(DIRECTORY_DATA, filename)
-
-    # Print message
-    print(f"Downloading data from the {datacenter} datacenter.")
-
-    # Set default parameters
-    kwargs.setdefault("network", "YA")
-    kwargs.setdefault("station", "UV*")
-    kwargs.setdefault("location", "*")
-    kwargs.setdefault("channel", "HHZ")
-
-    # Download data
-    stream = download_dataset(starttime=starttime, endtime=endtime, **kwargs)
 
     # Raise error if no data
     if stream is None:
         raise ValueError("No data found.")
 
-    # Resample data to 20 Hz
-    stream.resample(20)
-    stream.merge(method=-1)
-    stream.sort(keys=["station"])
+    # Preprocess
+    if process is not None:
+        map_processing_chain(stream, process)
+
+    # Raise error if no data
+    if stream is None:
+        raise ValueError("No data in stream.")
+
+    return stream
+
+
+def download_undervolc_data(
+    filepath_destination: str | None = None,
+    starttime: str | UTCDateTime = "2010-10-14T09:00:00",
+    endtime: str | UTCDateTime = "2010-10-14T16:00:00",
+    network: str = "YA",
+    station: str = "UV*",
+    location: str = "*",
+    channel: str = "HHZ",
+    datacenter: str = "RESIF",
+    process={
+        "resample": 20,
+        "merge": {"method": 1, "fill_value": 0},
+        "sort": {"keys": ["station"]},
+    },
+    **kwargs,
+) -> None:
+    """Download data from the UnderVolc network.
+
+    The argument are the same as the :func:`~covseisnet.data.download_seismic_dataset` with the following default values:
+
+    - ``starttime``: "2010-10-14T09:00:00"
+    - ``endtime``: "2010-10-14T16:00:00"
+    - ``network``: "YA"
+    - ``station``: "UV*"
+    - ``location``: "*"
+    - ``channel``: "HHZ"
+    - ``datacenter``: "RESIF"
+    - ``process``: resample to 20 Hz, merge the traces, and sort by station.
+
+    And a default value for the ``filepath_destination`` argument, which is set
+    to the ``/data`` repository located at the root of this project.
+
+    """
+    # Infer location
+    if filepath_destination is None:
+        filename = "undervolc_example.mseed"
+        filepath_destination = path.join(DIRECTORY_DATA, filename)
+
+    # Print message
+    print(f"Downloading data from the {datacenter} datacenter.")
+
+    # Download data
+    stream = download_seismic_dataset(
+        starttime=starttime,
+        endtime=endtime,
+        network=network,
+        station=station,
+        location=location,
+        channel=channel,
+        datacenter=datacenter,
+        process=process,
+        **kwargs,
+    )
+    print(stream)
 
     # Write stream
     stream.write(filepath_destination, format="MSEED", encoding="FLOAT64")
@@ -170,98 +224,59 @@ def download_undervolc_data(
 
 def download_usarray_data(
     filepath_destination: str | None = None,
-    starttime: UTCDateTime = UTCDateTime("2010-01-01"),
-    endtime: UTCDateTime = UTCDateTime("2010-03-01"),
+    starttime: str | UTCDateTime = "2006-01-01",
+    endtime: str | UTCDateTime = "2006-03-01",
     datacenter: str = "IRIS",
+    network: str = "TA",
+    station: str = "R04C,O03C,M03C,L02A,I05A",
+    location: str = "*",
+    channel: str = "LHZ",
+    process={
+        "merge": {"method": 1, "fill_value": 0},
+        "resample": 0.2,
+        "sort": {"keys": ["station"]},
+    },
     **kwargs,
 ) -> None:
-    """Download data from the UnderVolc network.
+    """Download data from the USArray network.
 
-    Arguments
-    ---------
-    starttime : :class:`~obspy.UTCDateTime`
-        The start time of the data to download.
-    endtime : :class:`~obspy.UTCDateTime`
-        The end time of the data to download.
+    The argument are the same as the :func:`~covseisnet.data.download_seismic_dataset` with the following default values:
+
+    - ``starttime``: "2006-01-01"
+    - ``endtime``: "2006-03-01"
+    - ``network``: "TA"
+    - ``station``: "R04C,O03C,M03C,L02A,I05A"
+    - ``location``: "*"
+    - ``channel``: "LHZ"
+    - ``datacenter``: "IRIS"
+    - ``process``: merge the traces, resample to 0.2 Hz, and sort by station.
+
+    And a default value for the ``filepath_destination`` argument, which is set
+    to the ``/data`` repository located at the root of this project.
     """
     # Infer location
     if filepath_destination is None:
-        filename = "example_usarray.mseed"
+        filename = "usarray_example.mseed"
         filepath_destination = path.join(DIRECTORY_DATA, filename)
 
     # Print message
     print(f"Downloading data from the {datacenter} datacenter.")
 
-    # Set default parameters
-    kwargs.setdefault("network", "TA")
-    kwargs.setdefault(
-        "station",
-        "BGNE,J28A,L27A,N23A,O28A,P33A,R27A,S32A,U29A,W31A",
+    # Download data
+    stream = download_seismic_dataset(
+        starttime=starttime,
+        endtime=endtime,
+        datacenter=datacenter,
+        network=network,
+        station=station,
+        location=location,
+        channel=channel,
+        process=process,
+        **kwargs,
     )
-    kwargs.setdefault("location", "*")
-    kwargs.setdefault("channel", "LHZ")
-
-    # Download data
-    stream = download_dataset(starttime=starttime, endtime=endtime, **kwargs)
-
-    # Raise error if no data
-    if stream is None:
-        raise ValueError("No data found.")
-
-    # Merge
-    stream.merge(method=-1)
-    stream.sort(keys=["station"])
 
     # Write stream
-    stream.write(filepath_destination, format="MSEED")
-
-    # Print message
-    print(f"Data saved to {filepath_destination}")
-
-
-def download_geoscope_data(
-    filepath_destination: str | None = None,
-    starttime: UTCDateTime = UTCDateTime("2020-05-01"),
-    endtime: UTCDateTime = UTCDateTime("2020-08-01"),
-    datacenter: str = "RESIF",
-    **kwargs,
-) -> None:
-    """Download data from the UnderVolc network.
-
-    Arguments
-    ---------
-    starttime : :class:`~obspy.UTCDateTime`
-        The start time of the data to download.
-    endtime : :class:`~obspy.UTCDateTime`
-        The end time of the data to download.
-    """
-    # Infer location
-    if filepath_destination is None:
-        filename = "example_geoscope.mseed"
-        filepath_destination = path.join(DIRECTORY_DATA, filename)
-
-    # Print message
-    print(f"Downloading data from the {datacenter} datacenter.")
-
-    # Set default parameters
-    kwargs.setdefault("network", "G")
-    kwargs.setdefault("station", "CLEV,CLF,CURIE,GOTF,HARD")
-    kwargs.setdefault("location", "*")
-    kwargs.setdefault("channel", "LHZ")
-
-    # Download data
-    stream = download_dataset(starttime=starttime, endtime=endtime, **kwargs)
-
-    # Raise error if no data
-    if stream is None:
-        raise ValueError("No data found.")
-
-    # Preprocess
-    stream.merge(method=-1)
-    stream.sort(keys=["station"])
-
-    # Write stream
-    stream.write(filepath_destination, format="MSEED")
+    stream.write(filepath_destination, format="MSEED", encoding="FLOAT64")
 
     # Print message
     print(f"Data saved to {filepath_destination}")
