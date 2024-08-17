a81ca00a3a726da2d4038d72f75f774334a453f9
Leonard Seydoux <seydoux@ipgp.fr>
2024-08-17 11:36:15 +0200

    Added a test for correlation matrix. Must add more

---
+++ tests/test_correlation.py
@@ -0,0 +1,15 @@
+"""Test of the ArrayStream class."""
+
+import numpy as np
+
+import covseisnet as csn
+
+
+def test_correlation_matrix_instance():
+    """Check various instances of the CovarianceMatrix class."""
+    stream = csn.NetworkStream.read()
+    *_, covariance = csn.calculate_covariance_matrix(
+        stream, window_duration=2, average=10
+    )
+    *_, correlation = csn.calculate_cross_correlation_matrix(covariance)
+    assert isinstance(correlation, csn.CrossCorrelationMatrix)
